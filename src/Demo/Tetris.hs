c827de1c64ccde10f39ebfdfbad6415016c46751
Frans <fjaspers@xs4all.nl>
2020-11-09 13:56:22 +0100

    Rebuilding tetris

--- src/Demo/Tetris.hs
+++ src/Demo/Tetris.hs
@@ -14,7 +14,8 @@ module Demo.Tetris
 import Linear.V2 (V2(..), _x, _y, perp)
 import Lens.Micro ((^.))
 import qualified Data.Map as Map (Map, fromList)
-import qualified Data.List as List (groupBy, sortOn, partition)
+import qualified Data.List as List (groupBy, sortOn, (\\))
+import qualified Data.Maybe as Maybe (isNothing)
 
 type Pos = V2 Int
 
@@ -27,59 +28,58 @@ data Block = Block
     { pos :: Pos                        -- Absolute position of blocks center.
     , poss :: [Pos]                     -- Relative positions (from center) of blocks constituents.
     , name :: String                    -- Blocks name. For widget attributes.
-    , status :: BlockStatus             -- is the block moving
     } deriving (Show)
 
-iBlock = Block { pos = V2 5 4, poss = [V2 0 (-2), V2 0 (-1), V2 0 0, V2 0 1], name = "iblock", status = Moving }
-oBlock = Block { pos = V2 10 2, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Moving }
-tBlock = Block { pos = V2 15 5, poss = [V2 (-1) (-1), V2 0 (-1), V2 0 0, V2 1 (-1)], name = "tblock", status = Moving }
-sBlock = Block { pos = V2 20 5, poss = [V2 0 (-1), V2 1 (-1), V2 (-1) 0, V2 0 0], name = "sblock", status = Moving }
-zBlock = Block { pos = V2 25 5, poss = [V2 (-1) (-1), V2 0 (-1), V2 0 0, V2 1 0], name = "zblock", status = Moving }
+iBlock = Block { pos = V2 5 4, poss = [V2 0 (-2), V2 0 (-1), V2 0 0, V2 0 1], name = "iblock" }
+oBlock = Block { pos = V2 10 2, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+tBlock = Block { pos = V2 15 5, poss = [V2 (-1) (-1), V2 0 (-1), V2 0 0, V2 1 (-1)], name = "tblock" }
+sBlock = Block { pos = V2 20 5, poss = [V2 0 (-1), V2 1 (-1), V2 (-1) 0, V2 0 0], name = "sblock" }
+zBlock = Block { pos = V2 25 5, poss = [V2 (-1) (-1), V2 0 (-1), V2 0 0, V2 1 0], name = "zblock" }
 
-lBlock = Block { pos = V2 32 0, poss = [V2 1 (-1), V2 0 (-1), V2 0 0, V2 1 0], name = "lblock", status = Moving }
-jBlock = Block { pos = V2 10 45, poss = [V2 0 (-2), V2 0 (-1), V2 0 0, V2 (-1) 0], name = "jblock", status = Dropped }
+lBlock = Block { pos = V2 32 0, poss = [V2 1 (-1), V2 0 (-1), V2 0 0, V2 1 0], name = "lblock" }
+jBlock = Block { pos = V2 10 45, poss = [V2 0 (-2), V2 0 (-1), V2 0 0, V2 (-1) 0], name = "jblock" }
 
-testBlocks = [ Block { pos = V2 0 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 2 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 4 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 6 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 10 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 12 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 14 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 16 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 18 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 20 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 22 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 24 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 26 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 28 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 30 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 32 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 34 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
+testBlocks = [ Block { pos = V2 0 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 2 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 4 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 6 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 10 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 12 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 14 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 16 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 18 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 20 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 22 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 24 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 26 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 28 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 30 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 32 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 34 49, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
 
-             , Block { pos = V2 0 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 2 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 4 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 6 46, poss = [V2 0 0, V2 1 0, V2 2 0, V2 3 0], name = "iblock", status = Dropped }
-             , Block { pos = V2 6 47, poss = [V2 0 0, V2 1 0, V2 2 0, V2 3 0], name = "iblock", status = Dropped }
-             , Block { pos = V2 10 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 12 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 14 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 16 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 18 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 20 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 22 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 24 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 26 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 28 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 30 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
-             , Block { pos = V2 34 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock", status = Dropped }
+             , Block { pos = V2 0 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 2 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 4 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 6 46, poss = [V2 0 0, V2 1 0, V2 2 0, V2 3 0], name = "iblock" }
+             , Block { pos = V2 6 47, poss = [V2 0 0, V2 1 0, V2 2 0, V2 3 0], name = "iblock" }
+             , Block { pos = V2 10 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 12 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 14 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 16 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 18 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 20 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 22 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 24 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 26 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 28 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 30 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
+             , Block { pos = V2 34 47, poss = [V2 0 (-1), V2 1 (-1), V2 0 0, V2 1 0], name = "oblock" }
              ]
 
 data Game = Game
     { cols :: Int
     , rows :: Int
-    , blocks :: [Block]
+    , block :: Maybe Block
     , wall :: [Pos]
     , gameover :: Bool
     , counter :: Int
@@ -89,7 +89,7 @@ initialGame :: Game
 initialGame = Game
     { cols = 36
     , rows = 50
-    , blocks = [iBlock, oBlock, tBlock, sBlock, zBlock, jBlock, lBlock] ++ testBlocks
+    , block = Just iBlock
     , wall = []
     , gameover = False
     , counter = 0
@@ -97,19 +97,20 @@ initialGame = Game
 
 
 moveGame :: TetrisDirection -> Game -> Game
-moveGame d g = let g' = g { wall = buildWall g ++ ground g}
-                   moving = filter ((== Moving) . status) $ blocks g'
-                   dropped = filter ((== Dropped) . status) $ blocks g'
-                in g' { blocks = map (moveBlock d g') moving ++ dropped }
+moveGame dir game = case block game of
+                      Nothing -> game
+                      Just block -> case moveBlock dir game block of
+                                      Nothing -> game { block = Nothing, wall = buildWall block (wall game) }
+                                      Just block' -> game { block = Just block' }
 
-moveBlock :: TetrisDirection -> Game -> Block -> Block
+moveBlock :: TetrisDirection -> Game -> Block -> Maybe Block
 moveBlock dir game block = let block' = case dir of
-                                           TetrisLeft -> moveCenter block (V2 (-1) 0)
-                                           TetrisRight -> moveCenter block (V2 1 0)
-                                           TetrisDown -> moveCenter block (V2 0 1)
-                                           TetrisUp -> rotate block
-                            in if inWall block' (wall game) then block { status = Dropped }
-                               else if inBounds block' 0 (cols game) then block' else block
+                                            TetrisLeft -> moveCenter block (V2 (-1) 0)
+                                            TetrisRight -> moveCenter block (V2 1 0)
+                                            TetrisDown -> moveCenter block (V2 0 1)
+                                            TetrisUp -> rotate block
+                            in if inWall block' $ wall game ++ ground game then Nothing
+                               else if inBounds block' 0 (cols game) then Just block' else Just block
 
 moveCenter :: Block -> V2 Int -> Block
 moveCenter b d = b { pos = pos b + d }
@@ -118,29 +119,20 @@ rotate :: Block -> Block
 rotate b = b { poss = map perp $ poss b }
 
 freeFall :: Game -> Game
-freeFall g = let wall = buildWall g ++ ground g
-                 moving = filter ((== Moving) . status) $ blocks g
-                 dropped = filter ((== Dropped) . status) $ blocks g
-              in g { wall = wall, blocks = map (freeFallBlock wall) moving ++ dropped }
+freeFall g = case block g of
+               Nothing -> g
+               Just b -> g { wall = fallWall b (wall g)}
 
-freeFallBlock :: [Pos] -> Block -> Block
-freeFallBlock w b = let b' = moveCenter b $ V2 0 1
-                        in if inWall b' w then b {status = Dropped} else freeFallBlock w b'
-
-freeFallBlock' :: ([Pos], [Block]) -> Block -> ([Pos], [Block])
-freeFallBlock' (w, bs) b = let b' = moveCenter b $ V2 0 1
-                           in if inWall b' w then (w ++ map (+ pos b) (poss b), b : bs)  else freeFallBlock' (w, bs) b'
-
+fallWall :: Block -> [Pos] -> [Pos]
+fallWall block wall = let block' = moveCenter block (V2 0 1)
+                      in if inWall block' wall then buildWall block wall else fallWall block' wall
 
 inBounds :: Block -> Int -> Int -> Bool
 inBounds b min max = all (>= min) xs && all (< max) xs
     where xs = map ((^._x) . (+ pos b)) (poss b)
 
-buildWall :: Game -> [Pos]
-buildWall g = concatMap absPos dropped
-    where
-        dropped = filter ((== Dropped) . status)  $ blocks g
-        absPos b = map (+ pos b) $ poss b
+buildWall :: Block -> [Pos] -> [Pos]
+buildWall block wall = wall ++ map (+ pos block) (poss block)
 
 ground :: Game -> [Pos]
 ground g = map (\x -> V2 x (rows g)) $ take (cols g) . iterate (+1) $ 0
@@ -149,33 +141,30 @@ inWall :: Block -> [Pos] -> Bool
 inWall b w = any ((`elem` w) . (+ pos b)) (poss b)
 
 tickGame :: Game -> Game
-tickGame g | gameover g = g
-           | otherwise = tick 1 . newBlock . shrinkWall . moveGame TetrisDown $ g
+tickGame game
+  | isGameOver game = game { gameover = True }
+  | Maybe.isNothing (block game) = newBlock game
+  | otherwise = tick 1 . newBlock . shrinkWall . moveGame TetrisDown $ game
 
 newBlock :: Game -> Game
 newBlock = id
 
+isGameOver :: Game -> Bool
+isGameOver game = False
+
 shrinkWall :: Game -> Game
-shrinkWall g = let grouped = List.groupBy (\v1 v2 -> v1 ^. _y == v2 ^. _y) . List.sortOn (^. _y) . buildWall $ g
-                   dels = concat $ filter ((== cols g) . length) grouped
-                   wall = ground g ++ concat (takeWhile ((/= cols g) . length) (reverse grouped))
-
-                   (dropped, moving) = List.partition (\b -> status b == Dropped) $ blocks g
-                   dropped' = map (delete dels) dropped
-                   delete ds b = b { poss = filter (not . (`elem` ds) . (+ pos b)) $ poss b }
-
-                   dropped'' = filter (not . null . poss) dropped'
-
-                   (_, dropped''') = foldl freeFallBlock' (wall, []) dropped''
-                in g { blocks = moving ++ dropped''' }
-
+shrinkWall game = let grouped = reverse . List.groupBy (\v1 v2 -> v1 ^. _y == v2 ^. _y) . List.sortOn (^. _y) $ wall game
+                      dels = concat $ filter ((== cols game) . length) grouped
+                      score = 10.0 ** fromIntegral (length (filter ((== cols game) . length) grouped))
+                      wall' = wall game List.\\ dels
+                   in game { wall = wall' }
 
 tick :: Int -> Game -> Game
 tick i g = g {counter = counter g + i}
 
 -- Map of all blocks, all positions with the blocks attr.
 posNameMap :: Game -> Map.Map Pos String
-posNameMap g = Map.fromList (concatMap posNameTpl (blocks g))
+posNameMap game = Map.fromList (maybe []  posNameTpl (block game))
 
 -- List off absolute positions of a block, in a tuple with the blocks attrName
 posNameTpl :: Block -> [(Pos, String)]
